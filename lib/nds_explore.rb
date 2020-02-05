79acd74aa0046374fd6aa30c4ddb6f6ab5242cc4
Jacky Lin <jacky.jia.lin@gmail.com>
2020-02-05 22:52:43 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -6,14 +6,18 @@ def pretty_print_nds(nds)
   pp nds
 end
 
+
+
 def print_first_directors_movie_titles
-   ss_movies = directors_database[0][:movies]
-  index = 0
-
-  while index < ss_movies.length do
-    titles = ss_movies[index][:title]
+  movies = directors_database[0][:movies]
+  # index  = 0 
+  # while index < movies.length do 
+  #   titles = movies[index][:title]
+  #   puts titles 
+  #   index += 1 
+  # end
+  for movie in movies
+    titles = movies[:title]
     puts titles
-    index +=1
   end
-end
-
+end 
